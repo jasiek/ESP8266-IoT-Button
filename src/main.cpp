86f1eef40235cb882b9e584a9d12ed5281e737a0
Jan Szumiec <jan.szumiec@gmail.com>
2019-01-11 22:26:39 +0000

    Sending messages over MQTT should work.

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,29 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
 
 WiFiManager wifiManager;
-char mqtt_server[40];
-char mqtt_port[5];
+WiFiClient clientRegular;
+WiFiClientSecure clientSecure;
+char mqtt_server[40] = "192.168.178.8";
+char mqtt_port[5] = "1883";
 char mqtt_username[40];
 char mqtt_password[40];
-char mqtt_ssl[3];
+char mqtt_ssl[3] = "no";
+MQTTClient mqtt(128);
 
 // Find out if we've been intentionally reset, or is this a regular power on.
 bool triggered() {
   struct rst_info *rinfo = ESP.getResetInfoPtr();
-  return (rinfo->reason == REASON_EXT_SYS_RST);
+  Serial.println(ESP.getResetInfo());
+  return (rinfo->reason == REASON_DEEP_SLEEP_AWAKE);
 }
 
 // Handle Wi-Fi configuration and any additional plugins we may want to configure.
 bool connect_to_network() {
+  // TODO: -> plugins
   WiFiManagerParameter p_mqtt_server("mqtt_server", "MQTT Hostname", mqtt_server, 40);
   WiFiManagerParameter p_mqtt_port("mqtt_port", "MQTT Port", mqtt_port, 5);
   WiFiManagerParameter p_mqtt_username("mqtt_username", "MQTT Username", mqtt_username, 40);
@@ -46,10 +53,26 @@ bool connect_to_network() {
 }
 
 void notify() {
+  // TODO: -> plugins
+  bool ssl = (strcmp(mqtt_ssl, "yes") == 0);
 
+  int i_mqtt_port = atoi(mqtt_port);
+  mqtt.begin(mqtt_server, i_mqtt_port, ssl ? clientSecure : clientRegular);
+  if (mqtt.connect("iot button")) {
+    Serial.println("connected");
+    if (mqtt.publish("devices/button", "click")) {
+      Serial.println("published");
+    }
+  }
 }
 
 void setup() {
+  pinMode(D0, INPUT_PULLUP);
+  if (digitalRead(D0)) {
+    wifiManager.resetSettings();
+    delay(1000);
+  }
+
   Serial.begin(115200);
   Serial.println("ESP8266-IoT-Button starting up...");
   // Have we been powered up, or this is an intentional reset?
